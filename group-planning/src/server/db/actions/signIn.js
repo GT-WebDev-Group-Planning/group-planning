222e00e2aa3ca2e652872eaeb355531e19195060
Amy <45153739+amtang325@users.noreply.github.com>
2024-01-20 13:29:42 -0500

    set up login routes

---
+++ group-planning/src/server/db/actions/signIn.js
@@ -0,0 +1,10 @@
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const axios = require("axios")
+const config = require("config")
+
+const signIn = async (req, res) => {
+
+}
+
+module.exports = signIn
