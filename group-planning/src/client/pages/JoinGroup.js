de3d4b4f15bc362678d449c6c45a489871d51637
Amy <45153739+amtang325@users.noreply.github.com>
2024-02-24 11:56:03 -0500

    added join group page

--- group-planning/src/client/pages/JoinGroup.js
+++ group-planning/src/client/pages/JoinGroup.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const JoinGroup = () => {
-  return (
-    <div>JoinGroup</div>
-  )
+    const [code, setCode] = useState('');
+    const navigate = useNavigate();
+
+    const handleAddGroup = () => {
+        // const data = {
+        //   code,
+        //   userEmail,
+        // };
+        // axios
+        //   .post('http://localhost:5000/joingroup', data)
+        //   .then(() => {
+        //     enqueueSnackbar('Group Joined Successfully', { variant: 'success' });
+        //     navigate('/group');
+        //   })
+        //   .catch((error) => {
+        //     enqueueSnackbar('Code does not exist or already in group', { variant: 'error' });
+        //     console.log(error);
+        // });
+    };
+    
+    const handleCodeChange = (event) => {
+        setCode(event.target.value);
+    };
+
+    return (
+        <div className="groups--centered join-group">
+      <h2>Join Group</h2>
+      <br />
+      <div className="groups--inputs-col">
+        <div className="groups--input-row">
+          <p className="groups--input-text">Group Code: </p>
+          <input
+            type="text"
+            value={code}
+            onChange={handleCodeChange}
+            className="groups--create-input"
+          />
+        </div>
+        <button
+          className="groups--create-button"
+          onClick={handleAddGroup}
+        >
+          Join Group
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default JoinGroup
